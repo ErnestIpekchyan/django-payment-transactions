9a7803d2964cb06d96c2523a7edc978412f8847d
ernest <wolf1080@yandex.ru>
2020-08-09 22:33:19 +0300

    add url for PaymentAPIView

--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from payments.views import UserRegistrationAPIView, UserTransactionsAPIView, CurrencyApiView
+from payments.views import UserRegistrationAPIView, UserTransactionsAPIView, CurrencyApiView, PaymentAPIView
 
 urlpatterns = [
     path('users/register/', UserRegistrationAPIView.as_view(), name='registration'),
     path('users/transactions/', UserTransactionsAPIView.as_view(), name='user_transactions'),
+    path('users/payments/transfer/', PaymentAPIView.as_view(), name='payment_transfer'),
     path('currencies/', CurrencyApiView.as_view(), name='currencies'),
 ]
