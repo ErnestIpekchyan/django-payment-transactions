3e602cbb2ee926b9070b67dc29f9ab8423a5898c
ernest <wolf1080@yandex.ru>
2020-08-09 16:28:33 +0300

    UserSerializer: add currency_type, balance_amount fields

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 
-from payments.models import User
+from payments.models import User, AccountCurrency
 
 
 class UserSerializer(serializers.ModelSerializer):
+    currency_type = serializers.ChoiceField(choices=AccountCurrency.CURRENCY_TYPE_CHOICES)
+    balance_amount = serializers.IntegerField(min_value=0)
+
     class Meta:
         model = User
-        fields = ['id', 'email', 'password', 'first_name', 'last_name']
+        fields = [
+            'id',
+            'email',
+            'password',
+            'first_name',
+            'last_name',
+            'currency_type',
+            'balance_amount',
+        ]
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
