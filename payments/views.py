2e3b34fcc79f3d50dfafaad26483a996f5644c70
ernest <wolf1080@yandex.ru>
2020-08-09 18:25:41 +0300

    add UserTransactionsAPIView

--- payments/views.py
+++ payments/views.py
@@ -1,7 +1,16 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListCreateAPIView
 
-from payments.serializers import UserRegistrationSerializer
+from payments.models import UserTransactionHistory
+from payments.serializers import UserRegistrationSerializer, UserTransactionHistorySerializer
 
 
 class UserRegistrationAPIView(CreateAPIView):
     serializer_class = UserRegistrationSerializer
+
+
+class UserTransactionsAPIView(ListCreateAPIView):
+    serializer_class = UserTransactionHistorySerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        return UserTransactionHistory.objects.filter(user_id=user_id)
