a83866292c8a6010f2833b2efe5e1e9211f8de44
ernest <wolf1080@yandex.ru>
2020-08-09 15:28:03 +0300

    add url for UserRegistrationAPIView

--- payments/urls.py
+++ payments/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from payments.views import UserRegistrationAPIView
+
 urlpatterns = [
+    path('users/register/', UserRegistrationAPIView.as_view(), name='registration'),
 ]
