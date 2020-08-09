e5623765fcc1b60ff0d6f7e731c48d6ceea831b2
ernest <wolf1080@yandex.ru>
2020-08-10 00:07:53 +0300

    add test_payment_from_usd_to_eur

--- payments/tests/test_payment_api_view.py
+++ payments/tests/test_payment_api_view.py
@@ -1,7 +1,9 @@
+from decimal import Decimal
+
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
-from payments.models import Currency, User, AccountCurrency
+from payments.models import Currency, User, AccountCurrency, UserTransactionHistory
 
 
 class PaymentViewTest(APITestCase):
@@ -95,3 +97,36 @@ class PaymentViewTest(APITestCase):
         response = self.client.post(self.url, data=data)
         result_data = {'non_field_errors': ['Счет не принадлежит текущему пользователю']}
         self.assertEqual(response.json(), result_data)
+
+    def test_payment_from_usd_to_eur(self):
+        usd_currency = Currency.objects.create(name='Евро', symbol='€', multiplicity=100, rate=1.151)
+        eur_currency = Currency.objects.create(name='Доллар', symbol='$', multiplicity=100, rate=1.357)
+        sender = self.register_user('a@a.ru', usd_currency, 1000)
+        recipient = self.register_user('b@b.ru', eur_currency)
+        sender_account = AccountCurrency.objects.get(user=sender, currency=usd_currency)
+        recipient_account = AccountCurrency.objects.get(user=recipient, currency=eur_currency)
+
+        self.client.force_login(sender)
+        data = {
+            'sender_account': sender_account.id,
+            'recipient_account': recipient_account.id,
+            'transfer_amount': 200,
+        }
+        response = self.client.post(self.url, data=data)
+        self.assertEqual(response.status_code, 201)
+
+        debit_payment = UserTransactionHistory.objects.filter(
+            user=sender, payment_type=UserTransactionHistory.DEBIT,
+        )
+        add_payment = UserTransactionHistory.objects.filter(
+            user=recipient, payment_type=UserTransactionHistory.ADD,
+        )
+        self.assertTrue(debit_payment)
+        self.assertTrue(add_payment)
+        self.assertEqual(debit_payment.last().transfer_amount, 200)
+        self.assertEqual(add_payment.last().transfer_amount, Decimal('235.79'))
+
+        sender_account.refresh_from_db()
+        recipient_account.refresh_from_db()
+        self.assertEqual(sender_account.balance_amount, Decimal('800'))
+        self.assertEqual(recipient_account.balance_amount, Decimal('235.79'))
